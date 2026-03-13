ffd5eb941daee93d8dd911450bff17f50fbeaddc
jayachandra-v77 <jayachandra.v986@gmail.com>
2026-03-13 17:54:20 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-INDEX_NAME = os.getenv("INDEX_NAME")
